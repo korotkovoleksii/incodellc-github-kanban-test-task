1af5dc4d4ccae141094b2f2c56217d18a59091e0
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-04-12 11:51:30 +0300

    Make project's structure

---
+++ src/core/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      Start
+    </div>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './core/App/App';
+import CssBaseline from '@mui/material/CssBaseline';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
