60d004577a0b3a29b8efa0746c9b1810336ba666
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-04-25 12:27:43 +0300

    Refactor

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -2,17 +2,26 @@ import { Box, Container } from '@mui/material';
 import './App.css';
 import SearchFormUrl from '../../modules/components/SearchFormUrl/SearchFormUrl';
 import CardBoard from '../../modules/components/CardBoard/CardBoard';
+import { persistor, store } from '../../shared/store';
+import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react';
 function App() {
   return (
-    <Box>
-      <Container>
-        <Box>
-          <SearchFormUrl />
-          <CardBoard />
-        </Box>
-      </Container>
-    </Box>
+    <>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Box>
+            <Container>
+              <Box>
+                <SearchFormUrl />
+                <CardBoard />
+              </Box>
+            </Container>
+          </Box>
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
 
